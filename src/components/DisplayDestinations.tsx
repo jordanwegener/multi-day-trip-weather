bc3319fa73eec5e8cb96817086b604c760dd8f18
Jordan Wegener <jwegener@dius.com.au>
2022-12-24 16:43:32 +1100

    fleshed out destination cards. destination destination

--- src/components/DisplayDestinations.tsx
+++ src/components/DisplayDestinations.tsx
@@ -9,23 +9,34 @@ type DisplayDestinationsProps = {
 
 function DisplayDestinations({ destinations }: DisplayDestinationsProps) {
   return (
-    <Grid textAlign={'center'}>
+    <Grid textAlign={"center"}>
       <h3>Your Trip</h3>
       {destinations.map((destination) => (
         <Box
           key={destination.id}
           border={"1px solid black"}
           borderRadius={5}
-          margin={'10px auto'}
+          margin={"10px auto"}
           padding={3}
-          width={"400px"}
-          alignSelf={'center'}
+          width={"50%"}
+          alignSelf={"center"}
+          flexDirection={"row"}
         >
-          <h4>Stop {destination.id}</h4>
-          <h4>{destination.name}</h4>
-          <p>{destination.description}</p>
-          <p>latitude: {destination.gpsCoords.lat}</p>
+          <Box borderBottom={'1px solid black'}>
+            <h4>Stop {destination.id}</h4>
+            <h4>{destination.name}</h4>
+            <p>{destination.description}</p>
+            <p>latitude: {destination.gpsCoords.lat}</p>
             <p>longitude: {destination.gpsCoords.lon}</p>
+          </Box>
+          <Box>
+            <h4>Weather</h4>
+            <p>Weather: placeholder</p>
+            <p>Temperature: placeholder</p>
+            <p>Feels Like: placeholder</p>
+            <p>Wind: placeholder</p>
+            <p>Humidity: placeholder</p>
+          </Box>
         </Box>
       ))}
     </Grid>
